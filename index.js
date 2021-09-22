8f20e50042dd1bfda773839a4b7fc0e35e035672
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-22 15:05:57 -0700

    add logic to create a new date object, then render the times based on AP and PM'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const body = document.querySelector("body");
 const photoCredit = document.querySelector(".photocredit");
 const timeDiv = document.querySelector(".container__time");
+const hourParagraph = document.createElement("p");
+const minuteParagraph = document.createElement("p");
+const secondParagraph = document.createElement("p");
+const clockDisplayParagraph = document.createElement("p");
 const cryptoDiv = document.querySelector(".container__crypto");
 const dogeCoinName = document.createElement("p");
 const dogeCoinThumb = document.createElement("img");
@@ -41,7 +45,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         console.log(data);
         console.log(data.market_data.current_price.usd);
         dogeCoinName.textContent = data.name;
-        dogeCoinThumb.src = data.image.thumb;
+        dogeCoinThumb.src = data.image.small;
         dogeCoinCurrentPrice.textContent = "$USD: " + data.market_data.current_price.usd.toFixed(2);
         cryptoDiv.append(dogeCoinName);
         cryptoDiv.append(dogeCoinThumb);
@@ -54,6 +58,61 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         cryptoDiv.append(dogeCoinName);
     })
 
-//build a clock
+//build a clock/current time display feature!
+
+const pmTimes = {
+    13: 1,
+    14: 2,
+    15: 3,
+    16: 4,
+    17: 5,
+    18: 6,
+    19: 7,
+    20: 8,
+    21: 9,
+    22: 10,
+    23: 11
+}
+
+function renderClock() {
+    const clock = new Date();
+    console.log(clock);
+    const hour = clock.getHours();
+    const minute = clock.getMinutes();
+    const second = clock.getSeconds();
+    if (hour >= 13) {
+        switch(hour) {
+            case 13: hour = pmTimes[13];
+                break;
+            case 14: hour = pmTimes[14];
+                break;    
+            case 15: hourParagraph.textContent = pmTimes[15];    
+                break;
+            case 16: hourParagraph.textContent = pmTimes[16];    
+                break;   
+            case 17: hourParagraph.textContent = pmTimes[17];    
+                break;  
+            case 18: hourParagraph.textContent = pmTimes[18];    
+                break;  
+            case 19: hourParagraph.textContent = pmTimes[19];    
+                break;  
+            case 20: hourParagraph.textContent = pmTimes[20];    
+                break;  
+            case 21: hourParagraph.textContent = pmTimes[21];    
+                break;  
+            case 22: hourParagraph.textContent = pmTimes[22];    
+                break;    
+            case 23: hourParagraph.textContent = pmTimes[23];    
+                break;      
+            }
+        }
+ 
+    clockDisplayParagraph.textContent = `${hour} : ${minute} : ${second}`;
+    timeDiv.append(clockDisplayParagraph); 
+    }
+
+renderClock();
+
+
 //api for quote?
 //weather api
