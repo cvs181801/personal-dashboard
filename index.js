49c0882b8e7f8873e31b353a12c7590e70f56af6
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-21 18:41:23 -0700

    add comment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res=>res.json())
-    .then(data=>console.log(data.user.name, data.user.portfolio_url, data.user.bio));
+// fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+//     .then(res=>res.json())
+//     .then(data=>console.log(data.user.name, data.user.portfolio_url, data.user.bio));
+
+//this is scrimba's unsplash random image 'get ' link
