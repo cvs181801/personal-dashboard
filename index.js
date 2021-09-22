e97bd16bc32a3b36824cdf678c89cff3ec20b68f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-21 18:54:44 -0700

    add basic structure and some baby language code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-// fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-//     .then(res=>res.json())
-//     .then(data=>console.log(data.user.name, data.user.portfolio_url, data.user.bio));
+//get elements from the DOM
 
 //this is scrimba's unsplash random image 'get ' link
+//get the image from the api and diisplay it as background, along with the photographer
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res=>res.json())
+    .then(data=>console.log(data.user.name, data.user.portfolio_url, data.user.bio));
+
+//build a clock
+//api for quote?
+//weather api
