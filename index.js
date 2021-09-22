42dda5c648dc38a3915274d31fc73c8ea589d474
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-21 18:39:14 -0700

    experiment with accessing the photogra[her name, twitter link , bio

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//fetch the image from Unsplash
-
-fetch("https://unsplash.com/photos/NRQV-hBF10M")
-    .then(response => response.json())
-    .then(data => console.log(data));
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res=>res.json())
+    .then(data=>console.log(data.user.name, data.user.portfolio_url, data.user.bio));
