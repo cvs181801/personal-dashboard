c9eefdf27307916beca299dd463a0b09001570b8
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-22 10:27:45 -0700

    add the background image and the photo credit within the fetch api block

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //get elements from the DOM
+const body = document.querySelector("body");
+const photoCredit = document.querySelector(".photocredit");
 
 //this is scrimba's unsplash random image 'get ' link
 //get the image from the api and diisplay it as background, along with the photographer
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res=>res.json())
-    .then(data=>console.log(data.user.name, data.user.portfolio_url, data.user.bio));
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+        body.style.backgroundImage = `url('${data.urls.full}')`;
+        photoCredit.textContent = `Photo by: ${data.user.name}. About ${data.user.name}: ${data.user.bio}`;
+    })//data.urls.full); //data.user.name, data.user.portfolio_url, data.user.bio
 
 //build a clock
 //api for quote?
